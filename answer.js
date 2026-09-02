44b80510691527b31581856d169ca5dea085e7e7
Sangma83 <poushalisangma13@gmail.com>
2026-09-03 03:53:38 +0600

    question 5 solved

--- answer.js
+++ answer.js
@@ -76,5 +76,25 @@ getCngFare(2);
 
 
 const getChaseVerdict = (target, scored, ballsLeft) => {
+let runsNeeded = target - scored;
 
+if(runsNeeded <= 0){
+  return "Won";
+}else if(ballsLeft <= 0){
+  return "Lost";
 }
+
+let requiredRate = (runsNeeded / ballsLeft) * 6;
+
+if(requiredRate <= 6){
+  return `Need ${runsNeeded} runs in ${ballsLeft} balls | Comfortable`;
+}else if(requiredRate > 6 && requiredRate <= 12){
+  return `Need ${runsNeeded} runs in ${ballsLeft} balls | Tough`;
+}else{
+  return `Need ${runsNeeded} runs in ${ballsLeft} balls | Almost Impossible`;
+}
+
+};
+
+
+getChaseVerdict(150, 149, 1);
