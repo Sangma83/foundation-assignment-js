64b91a282f275af9a16795b1c8808f2d73e2cd35
Sangma83 <poushalisangma13@gmail.com>
2026-09-02 23:38:09 +0600

    correction

--- answer.js
+++ answer.js
@@ -1,12 +1,12 @@
 // Question 1: Value Detective 
 
-// function describeValue(num){
-//   if(num === "" || num === 0 || num === null || num === undefined){
-//     return `${typeof(num)} | falsy`;
-//   }else{
-//     return `${typeof(num)} | truthy`;
-//   }
-// }
+function describeValue(num){
+  if(num === "" || num === 0 || num === null || num === undefined){
+    return `${typeof(num)} | falsy`;
+  }else{
+    return `${typeof(num)} | truthy`;
+  }
+}
 
 // describeValue("hello");
 
