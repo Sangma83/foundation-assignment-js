5948ff9117ac755d951742e86d457394e949ffaa
Sangma83 <poushalisangma13@gmail.com>
2026-09-02 23:35:54 +0600

    solved question 2

--- answer.js
+++ answer.js
@@ -1,19 +1,39 @@
 // Question 1: Value Detective 
 
-function describeValue(num){
-  if(num === "" || num === 0 || num === null || num === undefined){
-    return `${typeof(num)} | falsy`;
-  }else{
-    return `${typeof(num)} | truthy`;
-  }
-}
+// function describeValue(num){
+//   if(num === "" || num === 0 || num === null || num === undefined){
+//     return `${typeof(num)} | falsy`;
+//   }else{
+//     return `${typeof(num)} | truthy`;
+//   }
+// }
 
-describeValue("hello");
+// describeValue("hello");
 
 
 // Question 2: Bangladesh Weekend Machine
 
+function getDayType(day) {
+    day = day.toLowerCase();
 
+    switch (day) {
+        case "friday":
+        case "saturday":
+            return "Weekend";
+
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
+
+        default:
+            return "Invalid Day";
+    }
+}
+
+console.log(getDayType("Bandarban")); 
 
 // Question 3: Username Gatekeeper
 
